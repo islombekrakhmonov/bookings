3c0dbf6cda666fec1f389bc539d2d651cdb64878
islombekrakhmonov <islombek.rakhmanov00@gmail.com>
2023-08-01 13:37:08 +0500

    saving html

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # bookings
+
+This is the repository for my bookings and reservations project.
+
+-Built in Go version 1.20
+-Uses the [chi router](github.com/go-chi/chi) 
+-Uses [alex edwards SCS](github.com/alexedwards/scs/v2) session management
+-Uses [nosurf](github.com/justinas/nosurf)
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(SessionLoad)
 	mux.Get("/home", http.HandlerFunc(handlers.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	
 	return mux
 }
